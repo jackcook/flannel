89da7fe256041568273cc323f6405e0ff87efd2a
Jack Cook <hello@jackcook.com>
2021-12-05 09:14:14 -0500

    Update test script

--- test.py
+++ test.py
@@ -2,15 +2,24 @@ import numpy as np
 from annoy import AnnoyIndex
 from flannel import AnnoyIndex as FlannelIndex
 from tqdm import trange
+import sys
 
+n_iter = int(sys.argv[1]) if len(sys.argv) > 1 else 10
 f = 100
+n_items = 10000
+search_k = 2000
 
 old_scores = []
 new_scores = []
 
-for a in trange(10):
+it = range(n_iter)
+
+if n_iter != 1:
+    it = trange(n_iter)
+
+for a in it:
     np.random.seed(a)
-    X = np.random.randn(10000, f)
+    X = np.random.randn(n_items, f)
 
     t_old = AnnoyIndex(f, "angular")
     t = FlannelIndex(f, "angular")
@@ -28,17 +37,17 @@ for a in trange(10):
     t.load("test.ann")
 
     k = 10
-    gt_idx, gt_dist = t_old.get_nns_by_item(0, k, search_k=1000000, include_distances=True)
+    gt_idx, gt_dist = t_old.get_nns_by_item(0, k, search_k=n_items * 2, include_distances=True)
 
-    old_idx, old_dist = t_old.get_nns_by_item(0, k, search_k=2000, include_distances=True)
+    old_idx, old_dist = t_old.get_nns_by_item(0, k, search_k=search_k, include_distances=True)
     old_matches = len(set(gt_idx).intersection(set(old_idx)))
     old_score = old_matches / k
-    # print(f"Old score: {old_score}")
+    assert 0 in old_idx
 
-    new_idx, new_dist = t.get_nns_by_item(0, k, search_k=1000, include_distances=True)
+    new_idx, new_dist = t.get_nns_by_item(0, k, search_k=search_k // 2, include_distances=True)
     new_matches = len(set(gt_idx).intersection(set(new_idx)))
     new_score = new_matches / k
-    # print(f"New score: {new_score}")
+    assert 0 in new_idx
 
     old_scores.append(old_score)
     new_scores.append(new_score)
