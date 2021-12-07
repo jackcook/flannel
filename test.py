8ab156876e5da7761c2a3126c6670448645aba5b
Jack Cook <hello@jackcook.com>
2021-12-06 21:27:24 -0500

    Good results with glove

--- test.py
+++ test.py
@@ -1,14 +1,45 @@
 import numpy as np
+import pandas as pd
 from annoy import AnnoyIndex
 from flannel import AnnoyIndex as FlannelIndex
-from tqdm import trange
+from tqdm import trange, tqdm
 import sys
+import os
+
+if not os.path.exists("glove_vecs.npz"):
+    df = pd.read_csv("unigram_freq.csv")
+    freqs = {x.word: x.count for x in tqdm(df.itertuples())}
+
+    def load_glove_model(fname):
+        vecs = []
+        weights = []
+
+        with open(fname, 'r') as f:
+            for line in tqdm(f):
+                split_line = line.split()
+                word = split_line[0]
+                embedding = np.array(split_line[1:], dtype=np.float64)
+
+                if word not in freqs:
+                    continue
+                
+                vecs.append(embedding)
+                weights.append(freqs[word])
+
+        return vecs, weights
+
+    vecs, weights = load_glove_model("glove.twitter.27B/glove.twitter.27B.25d.txt")
+    np.savez("glove_vecs.npz", vecs=vecs, weights=weights)
+
+f = np.load("glove_vecs.npz")
+vecs = f["vecs"]
+weights = f["weights"]
 
 n_iter = int(sys.argv[1]) if len(sys.argv) > 1 else 10
 f = 25
-n_items = 5000
 n_trees = 10
-search_k = 200
+search_k = 1000
+n_items = vecs.shape[0]
 
 old_scores = []
 new_scores = []
@@ -20,51 +51,52 @@ if n_iter != 1:
 
 for a in it:
     np.random.seed(a)
-    X = np.random.randn(n_items, f)
 
     t_old = AnnoyIndex(f, "angular")
 
     for i in range(n_items):
-        t_old.add_item(i, X[i])
+        t_old.add_item(i, vecs[i])
     
     t_old.build(n_trees)
 
     t = FlannelIndex(f, "angular")
 
-    p = np.random.exponential(1.5, size=(n_items,))
+    p = weights / weights.sum()
+    perc = np.percentile(p, 99.4)
 
-    for i in range(n_items):
+    for i in trange(n_items):
         w = p[i]
         neighbors = []
 
-        if w > np.percentile(p, 98):
+        if w >= perc:
             neighbors = t_old.get_nns_by_item(i, 10, search_k=n_items * n_trees)
 
-        t.add_item(i, X[i], w, neighbors=neighbors)
+        t.add_item(i, vecs[i], w, neighbors=neighbors)
 
-    t.build(n_trees, 50, top_p=0.01, with_neighbors=True)
+    t.build(n_trees, 10, top_p=0.005, with_neighbors=True)
     t.save("test.ann")
 
     t = FlannelIndex(f, "angular")
     t.load("test.ann")
 
-    item_i = np.argmax(p)
+    for _ in range(1000):
+        item_i = np.random.choice(range(n_items), p=p / p.sum())
 
-    k = 10
-    gt_idx, gt_dist = t_old.get_nns_by_item(item_i, k, search_k=n_items * 2, include_distances=True)
+        k = 10
+        gt_idx, gt_dist = t_old.get_nns_by_item(item_i, k, search_k=n_items * 2, include_distances=True)
 
-    old_idx, old_dist = t_old.get_nns_by_item(item_i, k, search_k=search_k, include_distances=True)
-    old_matches = len(set(gt_idx).intersection(set(old_idx)))
-    old_score = old_matches / k
-    assert item_i in old_idx
+        old_idx, old_dist = t_old.get_nns_by_item(item_i, k, search_k=search_k, include_distances=True)
+        old_matches = len(set(gt_idx).intersection(set(old_idx)))
+        old_score = old_matches / k
+        assert item_i in old_idx
 
-    new_idx, new_dist = t.get_nns_by_item(item_i, k, search_k=search_k, clusters_p=0.5, include_distances=True)
-    new_matches = len(set(gt_idx).intersection(set(new_idx)))
-    new_score = new_matches / k
-    assert item_i in new_idx
+        new_idx, new_dist = t.get_nns_by_item(item_i, k, search_k=search_k, clusters_p=0.01, include_distances=True)
+        new_matches = len(set(gt_idx).intersection(set(new_idx)))
+        new_score = new_matches / k
+        assert item_i in new_idx
 
-    old_scores.append(old_score)
-    new_scores.append(new_score)
+        old_scores.append(old_score)
+        new_scores.append(new_score)
 
 print(f"Old score: {np.mean(old_scores)}")
 print(f"New score: {np.mean(new_scores)}")
