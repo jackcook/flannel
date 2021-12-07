1ba0b438dbe97975e42dc0f5f145e9f8a355d962
Jack Cook <hello@jackcook.com>
2021-12-07 13:13:17 -0500

    Save nearest results for quicker evaluation

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from flannel import AnnoyIndex as FlannelIndex
 from tqdm import trange, tqdm
 import sys
 import os
+import pickle
 
 if not os.path.exists("glove_vecs.npz"):
     df = pd.read_csv("unigram_freq.csv")
@@ -40,6 +41,7 @@ f = 25
 n_trees = 10
 search_k = 1000
 n_items = vecs.shape[0]
+k = 10
 
 old_scores = []
 new_scores = []
@@ -66,21 +68,33 @@ t.save("test.ann")
 t = FlannelIndex(f, "angular")
 t.load("test.ann")
 
+def get_gt_idx(item_i):
+    if item_i in nearest:
+        return nearest[item_i]
+    
+    gt_idx = t_old.get_nns_by_item(item_i, k, search_k=n_items * n_trees)
+    nearest[item_i] = gt_idx
+
+    return gt_idx
+
+nearest = {}
+
+if os.path.exists("nearest.pkl"):
+    nearest = pickle.load(open("nearest.pkl", "rb"))
+
 for a in it:
     np.random.seed(a)
 
     for _ in range(100):
         item_i = np.random.choice(range(n_items), p=p / p.sum())
+        gt_idx = get_gt_idx(item_i)
 
-        k = 10
-        gt_idx, gt_dist = t_old.get_nns_by_item(item_i, k, search_k=n_items * 2, include_distances=True)
-
-        old_idx, old_dist = t_old.get_nns_by_item(item_i, k, search_k=search_k, include_distances=True)
+        old_idx = t_old.get_nns_by_item(item_i, k, search_k=search_k)
         old_matches = len(set(gt_idx).intersection(set(old_idx)))
         old_score = old_matches / k
         assert item_i in old_idx
 
-        new_idx, new_dist = t.get_nns_by_item(item_i, k, search_k=search_k, clusters_p=0, include_distances=True)
+        new_idx = t.get_nns_by_item(item_i, k, search_k=search_k, clusters_p=0)
         new_matches = len(set(gt_idx).intersection(set(new_idx)))
         new_score = new_matches / k
         assert item_i in new_idx
@@ -90,3 +104,6 @@ for a in it:
 
 print(f"Old score: {np.mean(old_scores)}")
 print(f"New score: {np.mean(new_scores)}")
+
+with open("nearest.pkl", "wb") as f:
+    pickle.dump(nearest, f)
